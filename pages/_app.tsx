c872b7eeed0973f4feb6727a0750fe3052eac286
viniciusrosa444 <viniciusrosa444@gmail.com>
2021-09-22 13:27:19 -0400

    inital

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import { AppProps } from "next/app";
+import './styles/global.scss';
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Head  from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1 className="">
+        teste
+      </h1>
+    </>
+  )
+}
